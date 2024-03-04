5017a6105f5d4e740b313775334deb19ccb1aac9
Jan Allersma <jan@allersma.be>
2024-03-04 12:08:16 +0100

    Add boilerplate MAUI app

---
+++ src/MauiDesigner/MauiDesigner/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDesigner;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/MauiDesigner/MauiDesigner/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiDesigner;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
